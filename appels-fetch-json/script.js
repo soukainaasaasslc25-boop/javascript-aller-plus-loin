bc17eb4d8976c1179fb2e757fcacd6ab8a60276e
soukaina <soukaina.asaas.slc25@gmail.com>
2025-10-31 11:36:39 +0000

    practice

--- appels-fetch-json/script.js
+++ appels-fetch-json/script.js
@@ -3,21 +3,8 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => response.json())
   .then(data => {
     console.log("Données reçues :", data);
+    
   })
   .catch(error => {
     console.error("Erreur lors de la récupération :", error);
   });
-
-
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log("Status :", response.status); // ex: 200
-    console.log("OK ?", response.ok);         // ex: true
-    console.log("Type :", response.type);     // ex: 'cors'
-    return response.json();                   // Convertit en JSON
-  })
-  .then(data => console.log("Données :", data));
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response =>{
-    console.log()
-  })
