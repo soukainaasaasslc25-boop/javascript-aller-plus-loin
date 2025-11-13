488026ac83072753cbcb8b06947ded86c4025429
soukaina <soukaina.asaas.slc25@gmail.com>
2025-11-13 09:02:23 +0000

    finich prototype

--- prototype/script.js
+++ prototype/script.js
@@ -1,10 +1,9 @@
-
 const MeteoApi=[
-    {
-        key:"4eb3703790b356562054106543b748b2",
-        baseUrl:"https://api.openweathermap.org/data/2.5/weather"
+  {
+      key:"4eb3703790b356562054106543b748b2",
+      baseUrl:"https://api.openweathermap.org/data/2.5/weather"
 
-    }
+  }
 ];
 
 
@@ -15,61 +14,119 @@ const donneesMeteo=document.getElementById("Id-donnee-Meteo");
 
 
 btnRechercher.addEventListener("click",function (event) {
-    event.preventDefault();
-    let nomVille=inputVille.value;
-  if (nomVille.trim()!=="") {
-     let url=MeteoApi[0].baseUrl+"?q="+nomVille+"&appid="+MeteoApi[0].key+"&units=metric&lang=fr";
-    fetch(url)
-    .then(reponse=>reponse.json())
-    .then(data=>{
-        console.log("city",data);
-      const nom=document.createElement("h3");
-      nom.textContent=" ville :" +nomVille;
-      const temperature=document.createElement("p");
-      temperature.textContent=" temperatue :" +data.main.temp_max;  
-       const humidity=document.createElement("p");
-      humidity.textContent=" humidity :" +data.main.humidity; 
+  event.preventDefault();
+  donneesMeteo.innerHTML="";
+  let nomVille=inputVille.value;
+if (nomVille.trim()!=="") {
+   let url=MeteoApi[0].baseUrl+"?q="+nomVille+"&appid="+MeteoApi[0].key+"&units=metric&lang=fr";
+  fetch(url)
+  .then(reponse=>reponse.json())
+  .then(data=>{
+    console.log("city",data);
 
-      const desciption=document.createElement("p");
-      desciption.textContent=" description :" +data.weather[0].description; 
+    //si la ville non Trouvee
+    if (data.cod == "404") {
+      donneesMeteo.textContent = " city not found!";
+      return;
+    }
 
-    //   senset time
-let sunset = data.sys.sunset; // timestamp en secondes
+    //creation d affichage de Meteo
+    const nom=document.createElement("h2");
+    nom.classList.add("nom");
 
-let secondes = sunset % 86400;           // secondes depuis minuit UTC
-let heuresUTC = Math.floor(secondes / 3600);
-let minutes = Math.floor((secondes % 3600) / 60);
+    const temperature=document.createElement("p");
+    temperature.classList.add("temperature");
+     
+    const humidity=document.createElement("p");
+    humidity.classList.add("humidity");
 
-let heuresMaroc = (heuresUTC + 1) % 24;  // Maroc = UTC+1
+    const speed = document.createElement("p");
+    speed.classList.add("speed");
 
-let periode = heuresMaroc >= 12 ? "PM" : "AM";
-let heures12 = heuresMaroc > 12 ? heuresMaroc - 12 : (heuresMaroc === 0 ? 12 : heuresMaroc);
-let minutes2 = minutes < 10 ? "0" + minutes : minutes;
+    const desciption=document.createElement("p");
 
-let p = document.createElement("p");
-p.textContent = `Coucher du soleil : ${heures12}:${minutes2} ${periode}`;
+    const img=document.createElement("img");
+    img.src="https://openweathermap.org/img/wn/"+data.weather[0].icon+"@4x.png";
+    img.classList.add("img");
 
-       
-       const img=document.createElement("img");
-       img.src="https://openweathermap.org/img/wn/"+data.weather[0].icon+"@4x.png";
-      donneesMeteo.appendChild(nom);
-      donneesMeteo.appendChild(temperature);
-      donneesMeteo.appendChild(desciption);
-        donneesMeteo.appendChild(humidity);
-      humidity.style.color="pink";
-    donneesMeteo.appendChild(p);
-      donneesMeteo.appendChild(img);
-    
-    })
-    .catch(error=>{
 
-        console.log("erroooooooooooooor",error);
-    })
+    // l affichage recupiration
+    speed.textContent= "🌪️ "+data.wind.speed+" km/h";
+    temperature.textContent=+data.main.temp+"°";  
+    nom.textContent=nomVille;
+    humidity.textContent=" 💧" +data.main.humidity+" %";
+    desciption.textContent=data.weather[0].description;
+
+// sunset
+let cocherSolie = data.sys.sunset;
+let actsecondes = cocherSolie%86400;// 
+let heuresUTC= Math.floor(actsecondes/3600);
+let actMinutes= Math.floor((actsecondes%3600)/60);
+let pmEtAm= document.createElement("p");
+if(heuresUTC>=12){
+  pmEtAm.textContent="🌅"+heuresUTC+": "+actMinutes+" PM";
+}
+else{
+  pmEtAm.textContent=" 🌅"+heuresUTC+": "+actMinutes+" AM";
+}
+if(actMinutes<10){
+      pmEtAm.textContent="🌅"+heuresUTC+": "+"0"+actMinutes+" AM";
+
+}
+
+//sunrise
+let leverduSolier= data.sys.sunrise;
+let actullsecondes = leverduSolier%86400;
+let lesHeuresUTC= Math.floor(actullsecondes/3600);
+let actullMinutes= Math.floor((actullsecondes%3600)/60);
+let amEtPm= document.createElement("p");
+if(lesHeuresUTC>=12){
+  amEtPm.textContent="🌄"+lesHeuresUTC+"  : "+actullMinutes+" PM";
+}
+else{
+  amEtPm.textContent="🌄"+lesHeuresUTC+"  : "+actullMinutes+" AM";
+}
+if(actullMinutes<10){
+  amEtPm.textContent="🌄"+lesHeuresUTC+"  : "+"0"+actullMinutes+" AM";
+
+}
+
+//juste pour css//
+const div = document.createElement("div");
+div.classList.add("div");
+//___________________________//
+
+
+  donneesMeteo.appendChild(nom);
+  donneesMeteo.appendChild(img); 
+  donneesMeteo.appendChild(temperature);
+  donneesMeteo.appendChild(desciption);
+  div.appendChild(humidity);
+  div.appendChild(speed);
+  div.appendChild(pmEtAm);
+  div.appendChild(amEtPm);
+  donneesMeteo.appendChild(div);
+
+  
+  if(data.weather[0].description=="couvert" || data.weather[0].description=="nuageux"){
+    document.body.style.backgroundImage="url('image/rain.jpg')";
   }
-  else{
-    const error=document.createElement("p");
-    error.textContent="enter le nome d abord";
-    donneesMeteo.innerHTML="";
-    donneesMeteo.appendChild(error);
+  else if(data.weather[0].description=="ciel dégagé" || data.weather[0].description=="peu nuageux"){
+    document.body.style.backgroundImage="url('image/weather.jpg')";
   }
+  else if(data.weather[0].description== "légère pluie"){
+    document.body.style.backgroundImage="url('image/winter.jpg')";
+  }
+  })
+  .catch(error=>{
+
+      console.log("erroooooooooooooor",error);
+  })
+}
+else{
+  const error=document.createElement("p");
+  error.textContent="enter le nome d abord";
+  donneesMeteo.innerHTML="";
+  donneesMeteo.appendChild(error);
+}
 });
